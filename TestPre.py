fd5a468155c7d2d3b2f03cff3558aa89460e9a93
wonthechan <packerstorm@gmail.com>
2019-08-27 15:58:35 +0900

    유저별 구매 시각에 대한 기초통계량 변수 피쳐 생성

--- TestPre.py
+++ TestPre.py
@@ -309,4 +309,21 @@ class FeatureObj(object):
         self.user_df = pd.merge(self.user_df, self.temp_df_agg, how='left')
         print(self.user_df.shape)
 
+        print(" # 유저별 구매 시각 - 기초통계량 변수")
+
+        self.temp_df = self._data.loc[:,['prchs_tm_clsf_nm','new_id']]
+        # SUM
+        self.temp_df_agg = pd.pivot_table(self.temp_df,index=["new_id"],aggfunc=np.sum)
+        self.temp_df_agg.rename(columns={'prchs_tm_clsf_nm':'prchs_tm_clsf_nm_sum'},inplace=True)
+        # MEAN, MAX, MIN, STD, VAR  
+        self.temp_df_agg['prchs_tm_clsf_nm_mean']=pd.pivot_table(self.temp_df,index=["new_id"],aggfunc=np.mean)
+        self.temp_df_agg['prchs_tm_clsf_nm_max']=pd.pivot_table(self.temp_df,index=["new_id"],aggfunc=np.max)
+        self.temp_df_agg['prchs_tm_clsf_nm_min']=pd.pivot_table(self.temp_df,index=["new_id"],aggfunc=np.min)
+        self.temp_df_agg['prchs_tm_clsf_nm_std']=pd.pivot_table(self.temp_df,index=["new_id"],aggfunc=np.std)
+        self.temp_df_agg['prchs_tm_clsf_nm_var']=pd.pivot_table(self.temp_df,index=["new_id"],aggfunc=np.var)
+        self.temp_df_agg['prchs_tm_clsf_nm_nuniq']=pd.pivot_table(self.temp_df,index=["new_id"],aggfunc=pd.Series.nunique)
+        self.temp_df_agg.reset_index(level=0, inplace=True)
+        self.user_df = pd.merge(self.user_df, self.temp_df_agg, how='left')
+        print(self.user_df.shape)
+
         return self.user_df
